17e6bcb05e45d1667087fa6bf074eca640ff470a
GeorgeDiasakos <158078082+GeorgeDiasakos@users.noreply.github.com>
2024-04-25 23:18:59 +0100

    ..

--- terraform/container-odf.tf
+++ terraform/container-odf.tf
@@ -1,11 +1,9 @@
+
 # Install ODF (OpenShift Data Foundation)
 ##############################################################################
 resource "ibm_container_addons" "addons" {
   cluster           = ibm_container_vpc_cluster.roks_cluster.id
   resource_group_id = ibm_resource_group.group.id
-  addons {
-    name    = "vpc-block-csi-driver"
-  }
   addons {
     name            = "openshift-data-foundation"
     version         = "4.14.0"
@@ -13,7 +11,7 @@ resource "ibm_container_addons" "addons" {
     {
         "osdSize":"2000Gi",
         "numOfOsd":"1",
-        "osdStorageClassName":"ibmc-vpc-block-10iops-tier",
+        "osdStorageClassName":"ibmc-vpc-block-metro-10iops-tier",
         "odfDeploy":"true"
     }
     PARAMETERS_JSON
